7df1a47b6df3f3cf42883f8dd8299bccf469a504
Haya Khan <125740158+hayakhan-ai@users.noreply.github.com>
2026-06-08 09:57:25 +0500

    last commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
     try {
 
       const res = await axios.post(
-        "https://saul-monic-damon.ngrok-free.dev/chat",
+        "http://127.0.0.1:8000/chat",
         {
           message
         }
